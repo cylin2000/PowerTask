7faff1c20cd0ed8c4fd8429cb585296d1d8ca4e1
cylin2000 <cylin2000@163.com>
2016-12-01 17:37:17 +0800

    adjust the load sequence

--- PowerTask.ps1
+++ PowerTask.ps1
@@ -1,15 +1,12 @@
 $powerTaskPath = "$env:USERPROFILE\PowerTask"
 $wc = New-Object System.Net.WebClient
 $wc.Encoding = [System.Text.Encoding]::UTF8
-if(!(Test-Path $powerTaskPath)){
-    md $powerTaskPath | Out-Null
-}
+if(!(Test-Path $powerTaskPath)){ md $powerTaskPath | Out-Null }
+Write-Host 
+Write-Host "Loading PowerTask ..." -ForegroundColor Green
 $t = Get-Random
 $wc.DownloadFile("https://raw.githubusercontent.com/cylin2000/powertask/master/PowerTask.psm1?t=$t","$powerTaskPath\PowerTask.psm1")
 Import-Module "$powerTaskPath\PowerTask.psm1" -Force
-
-Write-Host 
-Write-Host "Loading PowerTask ..." -ForegroundColor Green
 Get-Command -Module PowerTask
 Write-Host 
 Write-Host "PowerTask Loaded Successfully" -ForegroundColor Green
