ab9ec06bff0f79c625d1febeca29778e0d8d143c
cylin2000 <cylin2000@163.com>
2016-12-01 16:29:01 +0800

    update powertask import module infor

--- PowerTask.ps1
+++ PowerTask.ps1
@@ -1,3 +1,5 @@
 $powerTaskPath = "$env:USERPROFILE\PowerTask"
 $webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile()
+$webClient.DownloadFile("https://raw.githubusercontent.com/cylin2000/powertask/master/PowerTask.psm1","$powerTaskPath\PowerTask.psm1")
+Import-Module "$powerTaskPath\PowerTask.psm1"
+Write-Host "PowerTask Loaded Successfully" -ForegroundColor Green
