9904019f1ae00853b0469948eb0e1f7624852ddf
cylin2000 <cylin2000@163.com>
2016-12-01 17:22:03 +0800

    add random to void cache

--- PowerTask.ps1
+++ PowerTask.ps1
@@ -4,7 +4,8 @@ $wc.Encoding = [System.Text.Encoding]::UTF8
 if(!(Test-Path $powerTaskPath)){
     md $powerTaskPath | Out-Null
 }
-$wc.DownloadFile("https://raw.githubusercontent.com/cylin2000/powertask/master/PowerTask.psm1","$powerTaskPath\PowerTask.psm1")
+$t = Get-Random
+$wc.DownloadFile("https://raw.githubusercontent.com/cylin2000/powertask/master/PowerTask.psm1?t=$t","$powerTaskPath\PowerTask.psm1")
 Import-Module "$powerTaskPath\PowerTask.psm1" -Force
 Write-Host "Loading PowerTask ..." -ForegroundColor Green
 Get-Command -Module PowerTask
