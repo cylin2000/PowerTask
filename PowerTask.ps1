55b01fd70c20022119c4226a6a0d88737db3959d
cylin2000 <cylin2000@163.com>
2016-12-01 17:22:53 +0800

    add space line

--- PowerTask.ps1
+++ PowerTask.ps1
@@ -7,6 +7,8 @@ if(!(Test-Path $powerTaskPath)){
 $t = Get-Random
 $wc.DownloadFile("https://raw.githubusercontent.com/cylin2000/powertask/master/PowerTask.psm1?t=$t","$powerTaskPath\PowerTask.psm1")
 Import-Module "$powerTaskPath\PowerTask.psm1" -Force
+
+Write-Host 
 Write-Host "Loading PowerTask ..." -ForegroundColor Green
 Get-Command -Module PowerTask
 Write-Host "PowerTask Loaded Successfully" -ForegroundColor Green
