2862b3a7694be5fcb4973297971711ed03d9117e
cylin2000 <cylin2000@163.com>
2016-12-07 11:10:15 +0800

    add Get-InstalledSoftware function

--- PowerTask.psm1
+++ PowerTask.psm1
@@ -68,4 +68,28 @@ function Expand-Zip {
     }
 }
 
+function Get-InstalledSoftware {
+    <#
+    .SYNOPSIS
+        Get Installed Softwares on this computer
+    .DESCRIPTION
+        This function will return the Installed Software list
+    .EXAMPLE
+        Get-InstalledSoftware
+    #>
+    $InstalledSoftware = Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ | Get-ItemProperty
+    IF (Test-path HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\){
+        $InstalledSoftware += Get-ChildItem HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ | Get-ItemProperty
+    }
+    IF (Test-path HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\) {
+        $InstalledSoftware += Get-ChildItem HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\ | Get-ItemProperty
+    }
+    #($InstalledSoftware | Where {$_.DisplayName -ne $Null -AND $_.SystemComponent -ne "1" -AND $_.ParentKeyName -eq $Null} |Select DisplayName).GetEnumerator() | Sort-Object {"$_"}
+    $list = $InstalledSoftware | Where {$_.DisplayName -ne $Null -AND $_.SystemComponent -ne "1" }
+    return $list
+    # if($list -ne  $Null){
+    #     $list|Select DisplayName,ParentDisplayName|Sort-Object {"$_"}
+    # }
+}
+
 Export-ModuleMember "*-*"
