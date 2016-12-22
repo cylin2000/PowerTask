f5c33a7a7623d537ef52519c9cc66846f0eebc6b
cylin2000 <cylin2000@163.com>
2016-12-22 09:24:48 +0800

    add Show-BalloonTip function

--- PowerTask.psm1
+++ PowerTask.psm1
@@ -171,6 +171,28 @@ function Invoke-Sql {
     return $dataSet
 }
 
+function Show-BalloonTip {
+[CmdletBinding()] 
+Param (
+    [Parameter(Mandatory=$true)]$Text,
+    [Parameter(Mandatory=$true)]$Title,
+    $Icon = 'Info',
+    $Timeout = $10000
+    )
+Process {
+    Add-Type -AssemblyName System.Windows.Forms
+    If ($PopUp -eq $null)  {
+        $PopUp = New-Object System.Windows.Forms.NotifyIcon
+                        }
+            $Path = Get-Process -Id $PID | Select-Object -ExpandProperty Path
+            $PopUp.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($Path)
+            $PopUp.BalloonTipIcon = $Icon
+            $PopUp.BalloonTipText = $Text
+            $PopUp.BalloonTipTitle = $Title
+            $PopUp.Visible = $true
+            $PopUp.ShowBalloonTip($Timeout)
+   }
+}
 
 function Get-DoubanMovieRate {
     param(
