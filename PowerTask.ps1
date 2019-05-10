b3ba2e4fd12ba7e8786e0493f66e8526917bedf0
Cai, Yun-Lin <yun-lin.cai@dxc.com>
2019-05-10 14:56:33 +0800

    load local powertask firstly

--- PowerTask.ps1
+++ PowerTask.ps1
@@ -1,11 +1,13 @@
 $powerTaskPath = "$env:USERPROFILE\PowerTask"
-$wc = New-Object System.Net.WebClient
-$wc.Encoding = [System.Text.Encoding]::UTF8
 if(!(Test-Path $powerTaskPath)){ md $powerTaskPath | Out-Null }
 Write-Host 
 Write-Host "Loading PowerTask ..." -ForegroundColor Green
-$t = Get-Random
-$wc.DownloadFile("https://raw.githubusercontent.com/cylin2000/powertask/master/PowerTask.psm1?t=$t","$powerTaskPath\PowerTask.psm1")
+if(!(Test-Path "$powerTaskPath\PowerTask.psm1")){
+    $t = Get-Random
+    $wc = New-Object System.Net.WebClient
+    $wc.Encoding = [System.Text.Encoding]::UTF8
+    $wc.DownloadFile("https://raw.githubusercontent.com/cylin2000/powertask/master/PowerTask.psm1?t=$t","$powerTaskPath\PowerTask.psm1")
+}
 Import-Module "$powerTaskPath\PowerTask.psm1" -Force
 Get-Command -Module PowerTask
 Write-Host 
