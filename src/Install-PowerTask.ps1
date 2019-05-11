37bdcc421338bc9b8269dac393fab9d5b26a4260
Cai, Yun-Lin <yun-lin.cai@dxc.com>
2019-05-11 09:29:54 +0800

    update install-powertask to only install it

--- src/Install-PowerTask.ps1
+++ src/Install-PowerTask.ps1
@@ -9,22 +9,12 @@ function Install-PowerTask{
         Install-PowerTask
     #>
 
-    $powerTaskPath = "$env:USERPROFILE\PowerTask"
-    Write-Host "Get Latest PowerTask"
-    $t = Get-Random
-    $wc = New-Object System.Net.WebClient
-    $wc.Encoding = [System.Text.Encoding]::UTF8
-    $wc.DownloadFile("https://raw.githubusercontent.com/cylin2000/powertask/master/PowerTask.psm1?t=$t","$powerTaskPath\PowerTask.psm1")
-    
-    Import-Module "$powerTaskPath\PowerTask.psm1" -Force
-    Get-Command -Module PowerTask
-
     $profile = "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"
+    Write-Host "Installing PowerTask to profile" -ForegroundColor Green
     Set-Content $profile '$powerTaskPath = "$env:USERPROFILE\PowerTask"'
     Add-Content $profile 'Write-Host "Loading Local PowerTask ..." -ForegroundColor Green'
     Add-Content $profile 'Import-Module "$powerTaskPath\PowerTask.psm1" -Force'
     Add-Content $profile 'Get-Command -Module PowerTask'
     Add-Content $profile 'Write-Host "PowerTask Loaded Successfully" -ForegroundColor Green'
-
     Write-Host "PowerTask Installed Successfully" -ForegroundColor Green
 }
