be4073a37b7070ca3bf034191278ece6a322909e
Cai, Yun-Lin <yun-lin.cai@dxc.com>
2019-05-11 09:09:30 +0800

    add Install-PowerTask command

--- PowerTask.ps1
+++ PowerTask.ps1
@@ -2,12 +2,10 @@ $powerTaskPath = "$env:USERPROFILE\PowerTask"
 if(!(Test-Path $powerTaskPath)){ md $powerTaskPath | Out-Null }
 Write-Host 
 Write-Host "Loading PowerTask ..." -ForegroundColor Green
-if(!(Test-Path "$powerTaskPath\PowerTask.psm1")){
-    $t = Get-Random
-    $wc = New-Object System.Net.WebClient
-    $wc.Encoding = [System.Text.Encoding]::UTF8
-    $wc.DownloadFile("https://raw.githubusercontent.com/cylin2000/powertask/master/PowerTask.psm1?t=$t","$powerTaskPath\PowerTask.psm1")
-}
+$t = Get-Random
+$wc = New-Object System.Net.WebClient
+$wc.Encoding = [System.Text.Encoding]::UTF8
+$wc.DownloadFile("https://raw.githubusercontent.com/cylin2000/powertask/master/PowerTask.psm1?t=$t","$powerTaskPath\PowerTask.psm1")
 Import-Module "$powerTaskPath\PowerTask.psm1" -Force
 Get-Command -Module PowerTask
 Write-Host 
---
+++ src/Install-PowerTask.ps1
@@ -0,0 +1,30 @@
+function Install-PowerTask{
+    
+    <#
+    .SYNOPSIS    
+        Install Latest PowerTask
+    .DESCRIPTION 
+        Install Latest PowerTask
+    .EXAMPLE     
+        Install-PowerTask
+    #>
+
+    $powerTaskPath = "$env:USERPROFILE\PowerTask"
+    Write-Host "Get Latest PowerTask"
+    $t = Get-Random
+    $wc = New-Object System.Net.WebClient
+    $wc.Encoding = [System.Text.Encoding]::UTF8
+    $wc.DownloadFile("https://raw.githubusercontent.com/cylin2000/powertask/master/PowerTask.psm1?t=$t","$powerTaskPath\PowerTask.psm1")
+    
+    Import-Module "$powerTaskPath\PowerTask.psm1" -Force
+    Get-Command -Module PowerTask
+
+    $profile = "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"
+    Set-Content $profile '$powerTaskPath = "$env:USERPROFILE\PowerTask"'
+    Add-Content $profile 'Write-Host "Loading PowerTask ..." -ForegroundColor Green'
+    Add-Content $profile 'Import-Module "$powerTaskPath\PowerTask.psm1" -Force'
+    Add-Content $profile 'Get-Command -Module PowerTask'
+    Add-Content $profile 'Write-Host "PowerTask Loaded Successfully" -ForegroundColor Green'
+
+    Write-Host "PowerTask Installed Successfully" -ForegroundColor Green
+}
